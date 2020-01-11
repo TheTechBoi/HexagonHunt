455341f3d4e0ce253c8ad4d70158772734914ecf
TheTechBoi <56732567+TheTechBoi@users.noreply.github.com>
2020-01-11 15:02:28 +0300

    Add files via upload

---
+++ 6genBulma.py
@@ -0,0 +1,153 @@
+#CYMURGHSS <3
+
+import numpy as np
+import cv2
+from networktables import NetworkTables
+
+
+#NetworkTables.initialize(server='roborio-6025-frc.local') 
+#table = NetworkTables.getTable("Vision") 
+
+cap = cv2.VideoCapture(0)
+
+cam_angle = 60
+cam_width = 1280
+cam_center = cam_width/2
+cam_centerAngle = cam_angle/2
+ratio = cam_angle / cam_width
+
+hexagon_w = 76.2
+hexagon_h = 76.2/2
+hexagon_percentage = int((hexagon_h/hexagon_w)*100)
+minHexW = 150
+minHexH = 75
+hexagonVerification = False
+
+x_difference = 0
+angle_difference = 0
+
+#renkler
+lower_color =  np.array([28, 100, 100])
+upper_color =  np.array([48, 255, 255])
+
+def maxPixel(liste, index):
+    number = 0
+    for i in range(len(liste)):
+        if number < liste[i][0][index]:
+            number = liste[i][0][index]
+
+    return number
+
+def minPixel(liste, index):
+    number = 0
+    for i in range(len(liste)):
+        if number > liste[i][0][index]:
+            number = liste[i][0][index]
+
+    return number
+
+
+while 1:
+    _, frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    kernel = np.ones((15,15),np.float32)/225
+    
+    smoothed = cv2.filter2D(hsv,-1,kernel)
+
+    hsv_blur = cv2.medianBlur(smoothed,15)
+    
+
+    
+    mask = cv2.inRange(hsv_blur, lower_color, upper_color)
+
+    mask = cv2.erode(mask,kernel,iterations =2)
+    mask = cv2.dilate(mask,kernel, iterations=2) 
+
+    res = cv2.bitwise_and(frame,frame, mask = mask)
+
+    gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY) 
+    
+    #Yukarıda: Gerekli  format değiştirilmesi maskeleme blurlama vb.
+    
+    
+    #Sınırları belirleme
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None 
+
+    #frame2 = frame.copy()
+    #cv2.drawContours(frame2, cnts, -1 ,(0,255,0), 3) 
+
+    if len(cnts) > 0:
+
+            
+            c = max(cnts,key =cv2.contourArea) #buyuk değerin seçimi
+
+            maxX = maxPixel(c,0)
+            minX = minPixel(c,0)
+            maxY = maxPixel(c,1)
+            minY = minPixel(c,1)
+            
+            shapeW = maxX - minX
+            shapeH = maxY - minY
+
+            if shapeW >= minHexW and shapeH >= minHexH:
+                print('W is:')
+                print(shapeW)
+                print('H is:')
+                print(shapeH)
+                shape_percentage = int((shapeH/shapeW)*100)
+
+                if (hexagon_percentage - 5) <= shape_percentage <= (hexagon_percentage + 5):
+                    hexagonVerification = True
+                else:
+                    hexagonVerification = False
+
+
+            if hexagonVerification == True:
+                x = minX + int(shapeW/2)
+                y = minY + int(shapeH/2)
+                
+                cv2.circle(frame,(x,y), 6, (255, 0, 0), -1)
+                
+                x_difference = cam_center - x
+                angle_difference = 0-(x_difference*ratio)
+
+            else:
+                x = 0
+                y = 0
+                angle_difference = 0
+
+    else:
+        x = 0
+        y = 0
+        angle_difference = 0
+
+    print("x : ")
+    print(x)
+    print("y : ")
+    print(y) 
+    print("Angle : ")
+    print(angle_difference)
+
+    #table.putNumber("X", x)
+    #table.putNumber("Y", y)
+    #table.putNumber("Angle",angle_difference)
+    
+        
+    cv2.imshow('OHA',frame)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27: 
+        break
+
+
+        
+
+    
+    
+      
+ 
+cv2.destroyAllWindows() 
+cap.release()
